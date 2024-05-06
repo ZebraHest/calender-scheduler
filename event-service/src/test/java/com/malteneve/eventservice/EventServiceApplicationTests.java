dd227c8f21d3307f085210969795e78b9eaef1e6
Malte Neve-Græsbøll <malte.neve-graesboell@keylane.com>
2024-05-06 13:44:20 +0200

    Added Event-Service

---
+++ event-service/src/test/java/com/malteneve/eventservice/EventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malteneve.eventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
